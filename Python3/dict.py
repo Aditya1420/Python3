d0cd4a4b9f5ef0f2b922bd8bdb9567ba3b0384b8
Aditya Damar <adityadamar2000@gmail.com>
2022-07-09 19:53:33 +0530

    Added exercise file

--- Python3/dict.py
+++ Python3/dict.py
@@ -6,19 +6,21 @@ d2={"Aditya":"Damar","Amit":"Das","Souvik":"Sarkar","Vikash":{"MiddleName":"Kuma
 # d2["Angela"]="White"
 # print(d2)
 # del d2["Angela"]
-#
+print(d2["Aditya"])
+
+
 d2.update({"Anuj":"Bathary"})
 #print(d2)
 # print(d2.keys())
-# print(d2.values())
+#print(d2.values())
 # print(d2.items())
 
-d2=iter(d2)
-print(next(d2))
-print(next(d2))
-print(next(d2))
-print(next(d2))
-print(next(d2))
-print("hello")
-
-print("aditya")
+# d2=iter(d2)
+# print(next(d2))
+# print(next(d2))
+# print(next(d2))
+# print(next(d2))
+# print(next(d2))
+# print("hello")
+#
+# print("aditya")
---
+++ Python3/dictexercise.py
@@ -0,0 +1,11 @@
+#Creating dictionary of linux commands and adding meaning of the words
+
+words={"pwd":"Will show present directory",
+       "ls":"list all items in directory",
+       "cd":"to change directory",
+       "rm":"will remove all the files",
+       "vim":"to open file in editor mode",
+       "touch":"to create file",
+       "mkdir":"to create directory"}
+searchWord=input("Enter the word you want to search:") #taking input from user
+print(words[searchWord]) #printing the meaning of the given word
