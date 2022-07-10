c0918ff17d2f0ae6085ec31c61ff3790dcb10f24
Aditya Damar <adityadamar2000@gmail.com>
2022-07-10 14:54:38 +0530

    Add

--- Python3/Strings/strconcate.py
+++ Python3/Strings/strconcate.py
@@ -6,7 +6,14 @@
 #
 # print(10 * lastStr) # Using * to print same multiple times
 #########################################################################################################
-#String.format() to add new string without using concatenation
+
+#To add new string without using concatenation
+#(1).format()method
+#(2)f-strings(formatted string literals
 
 
+print("This is a string {}".format("Added"))#--Grab the string which needs to be inserted and add it to curly braces.
 
+print("This is a string {1} {0}".format("Added","Hello"))#-->Can put index values to add string accordingly
+
+print("The {q} {b} {f}".format(f="fox",b="brown",q="quick"))#-->Can assign variable for easy read
