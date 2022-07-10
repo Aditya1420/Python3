c08dc8b6019dc792c0c4df77ccdb448df77d779d
Aditya Damar <adityadamar2000@gmail.com>
2022-07-11 00:55:06 +0530

    Adding new data

--- Python3/Lists/listdt.py
+++ Python3/Lists/listdt.py
@@ -1,4 +1,21 @@
-num=[10,8,9,7,6,4]
+num=[10,1,2,5,8,9,7,6,4]
+num.sort()
+print(num)
+num.reverse()
+print(num)
+
+
+
+#print(num[1:])#-->list also supports slicing
+
+#List is mutable, we can change the data
+# list=["One","Two","Three"]
+# print(list)
+# list[0]="ONE all caps"
+# print(list)
+#
+# print(list+num)#Concatenation
+
 
 #print(len(num))
 # num.sort()
@@ -6,8 +23,8 @@ num=[10,8,9,7,6,4]
 # num1=int(input("Enter the new number:"))
 # num.append(num1)
 # num.sort()
-print(num)
+# print(num)
 
-tp=(1,2,3)
-#tp[0]=1
-print(tp)
+# tp=(1,2,3)
+# #tp[0]=1
+# print(tp)
