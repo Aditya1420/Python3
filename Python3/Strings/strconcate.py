c3e5cccbbde735b40c661521fc05926614e894eb
Aditya Damar <adityadamar2000@gmail.com>
2022-07-10 23:37:24 +0530

    Adding new data into strconcate.py

--- Python3/Strings/strconcate.py
+++ Python3/Strings/strconcate.py
@@ -1,8 +1,8 @@
 # To change string we can use concatenation
-# str="Aditya"
+# str="Damar"
 # lastStr=str[1:]
 #
-# print("D"+lastStr) # Using plus operator
+# print("A"+lastStr) # Using plus operator
 #
 # print(10 * lastStr) # Using * to print same multiple times
 #########################################################################################################
@@ -11,9 +11,28 @@
 #(1).format()method
 #(2)f-strings(formatted string literals
 
+#
+# print("This is a string {}".format("Added"))#--Grab the string which needs to be inserted and add it to curly braces.
+#
+# print("This is a string {1} {0}".format("Added","Hello"))#-->Can put index values to add string accordingly
+#
+# print("The {q} {b} {f}".format(f="fox",b="brown",q="quick"))#-->Can assign variable for easy read
+#
+# str=input("Enter the string:")
+# print("{} {}".format(str[0:5],str[6:]))
 
-print("This is a string {}".format("Added"))#--Grab the string which needs to be inserted and add it to curly braces.
 
-print("This is a string {1} {0}".format("Added","Hello"))#-->Can put index values to add string accordingly
+#float formatting follows "{value:width.percision f}"
+#-->value--that needs to print,width-->how long you want,precision-->place upto which it should print
 
-print("The {q} {b} {f}".format(f="fox",b="brown",q="quick"))#-->Can assign variable for easy read
+result=1/77
+print("The result {r:1.3f}".format(r=result))
+
+#f-string literal method
+
+name="Aditya"
+print(f"Hello, My name is {name}")#-->giving the variable in curly braces
+
+firstName=input("Enter first name:")
+lastName=input("Enter last name:")
+print(f"User full name is {firstName} {lastName}")
